fd3aed5338072b79173cbbfdbc293b69850198eb
girvel <widauka@ya.ru>
2026-03-02 10:38:07 +0500

    Allow primary, help message

--- cb.c
+++ cb.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <stdbool.h>
 
@@ -9,11 +10,35 @@ void assert_xclip(const FILE *xclip) {
     exit(1);
 }
 
-int main() {
+void help(const char *self) {
+    printf("Usage: %s [-p | --primary]\n", self);
+    printf("If piped in, sets clipboard; always prints it. Requires xclip.\n");
+    printf("\n");
+    printf("  -p, --primary    Use primary instead of cliboard\n");
+    printf("  -h, --help       Display help message and exit\n");
+}
+
+int main(int argc, char **argv) {
+    bool uses_primary = false;
+
+    for (int i = 1; i < argc; i++) {
+        char *arg = argv[i];
+        if (strcmp(arg, "--help") == 0 || strcmp(arg, "-h") == 0) {
+            help(*argv);
+            return 0;
+        } else if (strcmp(arg, "--primary") == 0 || strcmp(arg, "-p") == 0) {
+            uses_primary = true;
+        }
+    }
+
     char line[4096];
+    char command[64];
+    strcpy(command, uses_primary
+        ? "xclip -selection primary"
+        : "xclip -selection clipboard");
 
     if (!isatty(STDIN_FILENO)) {
-        FILE *xclip = popen("xclip -selection clipboard", "w");
+        FILE *xclip = popen(command, "w");
         assert_xclip(xclip);
 
         while (fgets(line, sizeof(line), stdin) != NULL) {
@@ -23,7 +48,8 @@ int main() {
         pclose(xclip);
     }
 
-    FILE *xclip = popen("xclip -o -selection clipboard", "r");
+    strcat(command, " -o");
+    FILE *xclip = popen(command, "r");
     assert_xclip(xclip);
 
     while (fgets(line, sizeof(line), xclip) != NULL) {
